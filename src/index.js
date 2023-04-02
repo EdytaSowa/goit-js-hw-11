f1e21f7b57ef2a99eb0869f56cfe6ec4d19a7d7e
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-04-02 10:24:18 +0200

    add pagination

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
 import { fetchImages } from './js/fetchImages';
+import { renderImages } from './js/renderImages';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 let lightbox = new SimpleLightbox('.gallery a');
 
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
 const gallery = document.querySelector('.gallery');
+const paginationBtn = document.querySelector('.load-more');
+
+let page = 1;
+paginationBtn.classList.add('hidden');
 
 btn.addEventListener('click', e => {
   e.preventDefault();
   const inputText = input.value;
-  console.log(inputText);
-  console.log(fetchImages(inputText));
 
-  fetchImages(inputText).then(image => renderImages(image));
+  fetchImages(inputText, page).then(({ data }) => {
+    gallery.innerHTML = '';
+
+    if (data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notiflix.Notify.success(`"Hooray! We found ${data.totalHits} images."`);
+      renderImages(data.hits);
+      paginationBtn.classList.remove('hidden');
+      lightbox.refresh();
+    }
+  });
 });
 
-const renderImages = images => {
-  if (images.length === 0) {
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
+paginationBtn.addEventListener('click', e => {
+  page++;
+  e.preventDefault();
+  paginationBtn.classList.add('hidden');
+  const inputText = input.value;
 
-  // Notiflix.Notify.success(`Hooray! We found total images.`);
-
-  const markup = images
-    .map(image => {
-      //   console.log(image);
-      return `
-<div class="photo-card">
- <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a> 
-  <div class="info">
-    <p class="info-item">
-      <b>Likes </b>  ${image.likes}
-    </p>
-    <p class="info-item">
-      <b>Views </b> ${image.views}
-    </p>
-    <p class="info-item">
-      <b>Comments </b> ${image.comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads </b> ${image.downloads}
-    </p>
-  </div>
-</div>`;
-    })
-    .join('');
-    gallery.innerHTML = markup;
-   
-    lightbox.refresh();
-};
+  fetchImages(inputText, page).then(({ data }) => {
+    if (page > Math.floor(data.hits / 40)) {
+      paginationBtn.classList.add('hidden');
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    } else {
+      renderImages(data.hits);
+      paginationBtn.classList.remove('hidden');
+      lightbox.refresh();
+    }
+  });
+});
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,28 +1,18 @@
 import axios from 'axios';
 
 const KEY = '30898532-07677536b584d5a486c7433f6';
+const baseUrl = 'https://pixabay.com/api/?key=';
 
-export const fetchImages = async text => {
+export const fetchImages = async (text, page) => {
   try {
-    const baseUrl = 'https://pixabay.com/api/?key=';
-    const firstResponse = await axios.get(
-      `${baseUrl}` +
-        `${KEY}` +
-        '&q=' +
-        `${text}` +
-        '&image_type=photo&orientation=horizontal&safesearch&per&per_page=40'
-    );
-    // console.log(firstResponse);
-    const images = firstResponse.data.hits;
-    // const totalHits = firstResponse.data.totalHits;
-
-    // console.log(totalHits);
-
-    // console.log(images);
-    return images;
+    const response = await axios.get(`
+      ${baseUrl}${KEY}&q=${text}&image_type=photo&orientation=horizontal&safesearch&per&per_page=40&page=${page}`);
+    return response;
   } catch (error) {
     console.error(error);
   }
 };
 
-fetchImages('red cat');
+
+
+
